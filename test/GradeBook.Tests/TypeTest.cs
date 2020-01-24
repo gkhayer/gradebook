f63610fac1595818ed652b1cb81b772d6286d4ff
gagan <gagan@ccpoabtf.org>
2020-01-24 15:33:45 -0800

    working on test: pass by value/reference

--- test/GradeBook.Tests/TypeTest.cs
+++ test/GradeBook.Tests/TypeTest.cs
@@ -5,22 +5,34 @@ namespace GradeBook.Tests
 {
     public class TypeTest
     {
+         [Fact]
+        public void CSharpisPassByvalue()
+        {
+            var book1 =  GetBook("Book 1");
+
+            GetBookSetName(book1, "New Name");
+            
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+        }
 
         [Fact]
-        public void Test1()
+        public void CanSetNameFromRefernce()
         {
             var book1 =  GetBook("Book 1");
 
             SetName(book1, "New Name");
             
-            Assert.Equal("Book 1", book1.Name);
-            Assert.Equal("Book 2", book2.Name);
-            Assert.NotSame(book1, book2);
+            Assert.Equal("New Name", book1.Name);
         }
 
-        private void SetName(Book book1, string v)
+        private void SetName(Book book, string name)
         {
-            
+            book.Name = name;
         }
 
         [Fact]
